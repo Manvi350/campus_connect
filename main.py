3fdceaa8aef7a65102985b080115663ed3b2548e
Manvi <manvi9539@gmail.com>
2026-05-05 15:43:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import auth
+from routes import auth,teacher_auth,teacher_routes,student_routes,admin_auth
 
 app = FastAPI()
 
@@ -11,10 +11,14 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(auth.router)
+app.include_router(teacher_auth.router)
+app.include_router(student_routes.router)
+app.include_router(teacher_routes.router)
+app.include_router(admin_auth.router)
