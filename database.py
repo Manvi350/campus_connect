99aa654c75f10a21ab225e73f5c244ffa5f464cc
Manvi <manvi9539@gmail.com>
2026-05-05 15:43:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,3 +8,10 @@ db = client["smart_campus"]
 
 # Collections
 student_collection = db["students"]
+teacher_collection=db["teachers"]
+admin_collection=db["admins"]
+attendance_collection = db["attendance"]
+assignment_collection = db["assignments"]
+# material_collection = db["materials"]
+# notice_collection = db["notices"]
+marks_collection = db["marks"]
